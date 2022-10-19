a2e3f85445c705c52ec54161346fa2628c881966
Subham Kumar <skuma251@asu.edu>
2022-10-19 00:50:12 -0700

    Implemeted the login logic based on the userType

--- FacadePatternCode/Facade.java
+++ FacadePatternCode/Facade.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Facade {
 
 	private int UserType;
@@ -10,52 +13,65 @@ public class Facade {
 
 	private Person thePerson;
 
-	public void login() {
+	public void invokeFacade() throws IOException {
+		System.out.println("Select the userType: \n 0: Buyer \n 1: Seller");
+		System.out.println("Enter userType:");
+		Scanner sc = new Scanner(System.in);
+		int UserType = Integer.parseInt(sc.next());
+		login(UserType);
+	}
+
+	public void login(int UserType) throws IOException {
+		Login user = new Login();
+		boolean loginStatus = user.login(UserType);
+		System.out.println(loginStatus);
 
 	}
 
-	public void addTrading() {
-
-	}
-
-	public void viewTrading() {
-
-	}
-
-	public void decideBidding() {
-
-	}
-
-	public void discussBidding() {
-
-	}
-
-	public void submitBidding() {
-
-	}
-
-	public void remind() {
-
-	}
-
-	public void createUser(UserInfoItem userInfoItem) {
-
-	}
-
-	public void createProductList() {
-
-	}
-
-	public void AttachProductToUser() {
-
-	}
-
-	public void SelectProduct() {
-
-	}
-
-	public void productOperation() {
-
-	}
+//	public void addTrading() {
+//
+//	}
+//
+//	public void viewTrading() {
+//
+//	}
+//
+//	public void decideBidding() {
+//
+//	}
+//
+//	public void discussBidding() {
+//
+//	}
+//
+//	public void submitBidding() {
+//
+//	}
+//
+//	public void remind() {
+//
+//	}
+//
+//	public void createUser(UserInfoItem userInfoItem) {
+//
+//	}
+//
+//	public void createProductList(int userType) {
+//		if(userType == 0) {
+//			Seller
+//		}
+//	}
+//
+//	public void AttachProductToUser() {
+//
+//	}
+//
+//	public void SelectProduct() {
+//
+//	}
+//
+//	public void productOperation() {
+//
+//	}
 
 }
